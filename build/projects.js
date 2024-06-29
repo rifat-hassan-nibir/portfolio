aa4c3439d15c3d815ca3d97d9dfa7f5e64c450f9
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2024-06-29 07:30:40 +0600

    change the name of dist folder to build

---
+++ build/projects.js
@@ -0,0 +1,20 @@
+const projects = [
+  {
+    id: "1",
+    image: "https://i.ibb.co/Ss7nmqn/talk-threads.png",
+    title: "Talk Threads",
+    description: "A forum web application where users can post blogs, share comments, and engage in discussions on various topics.",
+    technologies: ["React", "Tailwind CSS", "Framer Motion", "Firebase", "Express.js", "Node.js", "MongoDB"],
+    link: "https://talk-threads.web.app",
+  },
+  {
+    id: "2",
+    image: "https://i.ibb.co/VjSHgPR/Share-Plate.png",
+    title: "Share Plate",
+    description: "Community food sharing and surplus reduction platform where users can share and request for foods.",
+    technologies: ["React", "Tailwind CSS", "Framer Motion", "Firebase", "Express.js", "Node.js", "MongoDB"],
+    link: "https://share-plate-38a2e.web.app",
+  },
+];
+
+export default projects;
